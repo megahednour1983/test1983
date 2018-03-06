bb45255da7432abc421e62a23bf4bb71ed4c821a
root <root@megahed1.mylabserver.com>
2018-03-06 01:55:40 +0000

    Adding terraform file

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable region {
+default = "us-east-1"
+ description = "the aws region where we want create the resources"
+}
+
+variable myvpcid {
+default = "vpc-8c03aef7"
+ description = "you can choose the default vpc of the chosen region"
+}
